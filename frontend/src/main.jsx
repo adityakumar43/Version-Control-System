6052dfcc8fe59ace711b54b6f278e145b6e6d8fd
Aditya <kumaradityap123@gmail.com>
2026-09-10 23:09:15 +0530

    modified

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './authContext.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import ProjectRoutes from './Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <AuthProvider>
-    <App />
+    <Router>
+      <ProjectRoutes/>
+    </Router>
   </AuthProvider>
 )
